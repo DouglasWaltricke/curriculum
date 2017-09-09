56f932f20d60662c5f5b2182773d5d926482f0ad
Douglas Waltricke <douglas.waltricke@hotmail.com>
2017-09-08 23:38:42 -0300

    up

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.cross').hide();
+    $('.menu-mobile').hide();
+    $('.hamburger').click(function(){
+        $('.menu-mobile').slideToggle("slow",function(){
+            $('.cross').show();
+            $('.hamburger').hide();
+            
+        });
+        
+    });
+    $(".cross").click(function() {
+        $(".menu-mobile").slideToggle("slow", function() {
+            $(".cross").hide();
+            $(".hamburger").show();
+        });
+
+    });  
+});
